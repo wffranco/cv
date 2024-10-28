139653d81e988c683a2bd7fbd44e3bf452f87d59
Willem Franco <willemffrancoc@gmail.com>
2024-10-28 01:18:35 -0500

    Simplifying Dark mode handlers

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  darkMode: 'class',
+  darkMode: ['variant', [
+    '@media (prefers-color-scheme: dark) { &:not([data-theme="light"] *) }',
+    '&:is([data-theme="dark"] *)',
+  ]],
   theme: {
     extend: {
       fontFamily: {
