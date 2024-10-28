9e1e1acdbf6f0e37ee26c6856fdcf12337965baf
Willem Franco <willemffrancoc@gmail.com>
2024-10-28 01:19:20 -0500

    Adding vue.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,14 @@ import { defineConfig } from 'astro/config';
 
 import alpinejs from '@astrojs/alpinejs';
 import tailwind from '@astrojs/tailwind';
+import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind(),
     alpinejs(),
+    vue(),
   ],
   site: 'https://wffranco.github.io',
   base: 'cv',
